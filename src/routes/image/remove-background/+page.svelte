9512615db7d25aff7eefec91f26a7172138f40c5
Alfredo González Martínez <48945157+alfredogonzalezmartinez@users.noreply.github.com>
2023-03-11 21:07:18 +0100

    Use image tool constants in image background remover page

--- src/routes/image/remove-background/+page.svelte
+++ src/routes/image/remove-background/+page.svelte
@@ -1,12 +1,20 @@
 <script>
 	import ImagePicker from '$lib/components/ImagePicker/ImagePicker.svelte';
+	import ToolDisabledAlert from '$lib/components/Alerts/ToolDisabledAlert.svelte';
 	import ToolTitle from '$lib/components/ToolTitle/ToolTitle.svelte';
+	import { IMAGE_TOOL } from '$lib/Constants/ImageTools';
+
+	const backgroundRemover = IMAGE_TOOL.removeBackground;
 </script>
 
 <svelte:head>
-	<title>Image background remover</title>
+	<title>{backgroundRemover.title}</title>
 </svelte:head>
 
-<ToolTitle>Image background remover</ToolTitle>
+<ToolTitle>{backgroundRemover.title}</ToolTitle>
 
-<ImagePicker />
+{#if backgroundRemover.isEnabled}
+	<ImagePicker actionUrl={backgroundRemover.apiUrl} />
+{:else}
+	<ToolDisabledAlert />
+{/if}
